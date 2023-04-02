e593e3f22daaeb1b72c6ef6328ca33f7f89f144d
Raisa Rafi <rafi.raisaar@gmail.com>
2023-04-02 16:05:01 -0600

    Database Connection

---
+++ src/edu/ucalgary/oop/DatabaseConnection.java
@@ -0,0 +1,48 @@
+package edu.ucalgary.oop;
+
+import javax.xml.crypto.Data;
+import java.sql.*;
+
+public class DatabaseConnection {
+    private Connection dbConnect;
+    private ResultSet results;
+
+    public void createConnection() {
+        try {
+            dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/ewr", "oop", "password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String selectAnimals() {
+        StringBuffer animals = new StringBuffer();
+
+        try {
+            Statement statement = dbConnect.createStatement();
+            results = statement.executeQuery("SELECT * FROM animals");
+
+            while (results.next()) {
+                System.out.println("Animals: " + results.getString("AnimalNickname"));
+                animals.append(results.getString("AnimalNickname"));
+                animals.append('\n');
+            }
+            statement.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return animals.toString();
+    }
+
+    public static void main(String[] args) {
+        DatabaseConnection database = new DatabaseConnection();
+
+        database.createConnection();
+
+        String allAnimals = database.selectAnimals();
+        System.out.println("Here is a list of animal nicknames: ");
+        System.out.println(allAnimals);
+    }
+
+}
+
