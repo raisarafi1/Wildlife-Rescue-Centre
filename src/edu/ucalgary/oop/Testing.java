50d135e99d691b6bdbdaf2db6421e84794125bd5
joshua girma debele <joshdebele8@gmail.com>
2023-04-04 20:11:39 -0600

    Setting up Testing Boilerplate

--- src/edu/ucalgary/oop/Testing.java
+++ src/edu/ucalgary/oop/Testing.java
@@ -4,5 +4,40 @@ import static org.junit.Assert.*;
 import java.util.*;
 
 public class Testing {
-    
+    @Test
+    /*
+     The SpeciesTest will check and see if the animal that is being taken care
+     is part of the Enumeration class.
+     */
+    public void SpeciesExceptionTest() {
+        boolean passed = false;
+        try {
+            Animal animal = new Animal(20, 'dolphin', ELEPHANT);
+        }
+        catch(NotSpeciesException e) {
+            passed = true;
+        }
+        catch(Exception e) {}
+        assertTrue(passed);
+    }
+
+    public void SpeciesTest() {
+
+    }
+    @Test 
+    /*
+     The durationTest verifies that the duration is a positive non-zero number
+     */
+    public void durationTest() {
+
+    }
+
+    @Test
+    /*
+     The maxWindowTest verifies that the duration is a positive non-zero number
+     */
+    public void maxWindowTest() {
+
+    }
+
 }
